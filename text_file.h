8a3ebb8019da3ae00a4e774f38703441fae03a62
Kim Einar Larsen <kim.developer@gmail.com>
2024-10-28 16:32:51 +0100

    Update text_file.h

--- text_file.h
+++ text_file.h
@@ -10,9 +10,9 @@
 // NOTE: Since this text file utility is made for Windows,
 //       the newline character is \r\n instead of \n.
 // NOTE: Memory allocation is done on the user side. Not inside here.
-// NOTE: If you are using text_file_get_size(..) to get the size to allocate memory,
-//       then you don't need to add +1 in the size when allocating memory,
-//       otherwise remember to add +1 like this: text = malloc(size + 1);
+// NOTE: If you are using text_file_get_length(..) to get the length to allocate memory,
+//       then you don't need to add +1 in the length when allocating memory,
+//       otherwise remember to add +1 like this: text = malloc(length + 1);
 //       when dealing with strings (null terminated).
 //
 // TODO: Make this also to work with C++.
